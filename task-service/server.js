577c37efcfda6574a9f67417a4363e8c649b31b8
AdriCBueno <adrianacc.bueno@gmail.com>
2026-09-24 23:05:01 +0200

    set docker

--- task-service/server.js
+++ task-service/server.js
@@ -16,7 +16,7 @@ try {
   const database = client.db(dbName);
   const app = createApp(database.collection("tasks"));
 
-  app.listen(port, "127.0.0.1", () => {
+  app.listen(port, "0.0.0.0", () => {
     console.log(`API backend running at http://127.0.0.1:${port}`);
   });
 } catch (error) {
